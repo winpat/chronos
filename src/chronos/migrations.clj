f6a6146da2a845a8bb83ad58086ac4b2934143f6
Patrick Winter <patrickwinter@posteo.ch>
2022-12-17 13:33:47 +0100

    Check for missing migrations on startup

--- src/chronos/migrations.clj
+++ src/chronos/migrations.clj
@@ -1,5 +1,7 @@
 (ns chronos.migrations
   (:require [ragtime.repl :as repl]
+            [ragtime.protocols :refer [applied-migration-ids]]
+            [clojure.set :refer [difference]]
             [ragtime.jdbc :as ragtime]))
 
 
@@ -14,3 +16,13 @@
 
 (defn rollback [db-spec]
   (repl/rollback (generate-migration-config db-spec)))
+
+
+
+(defn pending-migrations
+  "Check if there are any pending migrations."
+  [db-spec]
+  (let [migrations (ragtime/load-resources "migrations")
+        migration-ids (set (map #(:id %) migrations))
+	    applied-migration-ids (set (applied-migration-ids (ragtime/sql-database db-spec {:migrations-table "migrations"})))]
+    (difference migration-ids applied-migration-ids)))
